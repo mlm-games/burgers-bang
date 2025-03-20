f5115a6bcfb31b07b7396be731ebefdab4599914
mlm-studios <you@example.com>
2025-03-20 19:34:07 +0530

    add burger recognition

--- burger.gd
+++ burger.gd
@@ -1,1 +1,10 @@
-extends MeshInstance3D
+class_name Burger extends MeshInstance3D
+
+var charging_throw: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+	
+			print("Recognised")
+			#global_position =  
+		
