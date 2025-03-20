6907cf3b6cfedb1b7d05b9a1a9fb231f633850c4
mlm-studios <you@example.com>
2025-03-20 22:14:10 +0530

    add initial test implementation for throwing of burger

--- burger.gd
+++ burger.gd
@@ -1,10 +1,1 @@
 class_name Burger extends MeshInstance3D
-
-var charging_throw: bool = false
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-	
-			print("Recognised")
-			#global_position =  
-		
--- gameplay.gd
+++ gameplay.gd
@@ -1,1 +1,37 @@
 extends Node3D
+
+var charging_throw: bool = false
+var throw_power : float = 0
+var burgers_thrown_count : int = 0
+
+@onready var current_burger: Burger = $InitialBurger
+
+func _input(event: InputEvent) -> void:
+	if current_burger == null:
+		return
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print("Recognised")
+			charging_throw = true
+		else:
+			charging_throw = false
+			throw_burger()
+		
+			##global_position =  
+			#
+	#if event is InputEventMouseMotion and charging_throw:
+		#var camera = get_viewport().get_camera_3d()
+		#var dir_ray = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.project_ray_origin(event.position),\
+			 #camera.project_ray_origin(event.position) + camera.project_ray_normal(event.position), 1))
+		#if dir_ray:
+			#current_burger.look_at((dir_ray.position - current_burger.global_transform.origin).normalized(), Vector3.UP)
+		
+func throw_burger() -> void:
+	current_burger.global_transform.origin += Vector3.ONE #HACK: test
+	#TODO: set burger collision layer.
+	burgers_thrown_count += 1
+	%BurgersThrownCountLabel.text = tr("Burgers thrown count: ") + str(burgers_thrown_count)
+	
+	#%BurgerRespawnTimer.start(); await %BurgerRespawnTimer.timeout
+	
