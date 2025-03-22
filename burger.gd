f93b4ed436545340b56e2538d36d61694d002804
mlm-studios <you@example.com>
2025-03-22 06:55:08 +0530

    spawn multiple burgers and mouse pos

--- burger.gd
+++ burger.gd
@@ -1,1 +1,10 @@
 class_name Burger extends RigidBody3D
+
+const BurgerScene = preload("uid://cns07x2ciq2dr")
+
+static func new_burger_at_position(pos: Vector3) -> Burger:
+	var burger_instance := BurgerScene.instantiate()
+	burger_instance.global_transform.origin = pos
+	#burger_instance.gravity_scale = 0.6
+	return burger_instance
+	
--- gameplay.gd
+++ gameplay.gd
@@ -5,7 +5,13 @@ var charging_throw: bool = false
 var throw_power : float = 1
 var burgers_thrown_count : int = 0
 
+@onready var viewport := get_viewport()
+@onready var mouse_position : Vector3
+@onready var camera := viewport.get_camera_3d()
+
 @onready var current_burger: Burger = $InitialBurger
+@onready var burger_spawn_point : Vector3 = current_burger.global_transform.origin
+
 
 func _input(event: InputEvent) -> void:
 	if current_burger == null:
@@ -18,21 +24,43 @@ func _input(event: InputEvent) -> void:
 		else:
 			charging_throw = false
 			throw_burger()
+			if %BurgerRespawnTimer.is_stopped(): %BurgerRespawnTimer.start()
 		
-			##global_position =  
+			
 			#
-	#if event is InputEventMouseMotion and charging_throw:
-		#var camera = get_viewport().get_camera_3d()
-		#var dir_ray = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.project_ray_origin(event.position),\
-			 #camera.project_ray_origin(event.position) + camera.project_ray_normal(event.position), 1))
-		#if dir_ray:
+	if event is InputEventMouseMotion and charging_throw:
+		var dir_ray : Dictionary = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.project_ray_origin(event.position),\
+			 camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)))
+		if !dir_ray: 
+			mouse_position = camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)
+		else:
+			mouse_position = dir_ray["position"]
+		print(mouse_position)
 			#current_burger.look_at((dir_ray.position - current_burger.global_transform.origin).normalized(), Vector3.UP)
 		
 func throw_burger() -> void:
+	current_burger.gravity_scale = 1
 	current_burger.apply_central_impulse(current_burger.global_transform.basis.z * throw_power)
+	current_burger.apply_central_impulse(current_burger.global_transform.basis.y * throw_power * 2)
 	#TODO: set burger collision layer.
 	burgers_thrown_count += 1
 	%BurgersThrownCountLabel.text = tr("Burgers thrown count: ") + str(burgers_thrown_count)
 	
 	#%BurgerRespawnTimer.start(); await %BurgerRespawnTimer.timeout
+	
+
+func _process(delta: float) -> void:
+	if charging_throw:
+		#throw_power = throw_power * ((delta + 100)/2)
+		current_burger.gravity_scale = -0.1
+	#else:
+		#throw_power = 1 #TODO: add it on new burger spawn
+		#current_burger.gravity_scale = 0.6
+		#current_burger.global_transform.origin = get_viewport().get_mouse_position()
+
+
+func _on_burger_respawn_timer_timeout() -> void:
+	current_burger = Burger.new_burger_at_position(burger_spawn_point)
+	add_child(current_burger)
+	
 	
