e5a5d113a477503697e1ff2acfed728dd58f70a3
mlm-studios <you@example.com>
2025-03-24 08:19:24 +0530

    invert angle and it works as intended

--- gameplay.gd
+++ gameplay.gd
@@ -18,6 +18,7 @@ var burgers_landed = 0
 #var power_applied : float = 0  #Power applied in x direction
 
 var swipe_direction: float = 0
+var reference_ray : Dictionary
 
 @onready var viewport := get_viewport()
 @onready var mouse_position : Vector3
@@ -28,33 +29,28 @@ var swipe_direction: float = 0
 
 
 func _input(event: InputEvent) -> void:
-	get_viewport().get_mouse_position()
 	if current_burger == null:
+		charging_throw = false
 		return
 	
 	if event is InputEventMouseButton:
-		if event.is_pressed():
-			print("Recognised")
-			charging_throw = true
-			#initial_swipe_pos = mouse_position
-		else:
-			charging_throw = false
-			throw_burger()
-			if %BurgerRespawnTimer.is_stopped(): %BurgerRespawnTimer.start()
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				reference_ray = shoot_ray(event)
+				print("Recognised")
+				charging_throw = true
+				#initial_swipe_pos = mouse_position
 		
 			
-			#Extra info: https://stackoverflow.com/questions/76893256/how-to-get-the-3d-mouse-pos-in-godot-4-1
 	if event is InputEventMouseMotion and charging_throw:
-		pass
+		await get_tree().create_timer(0.1).timeout
+		if charging_throw and shoot_ray(event).position != reference_ray.position and current_burger != null: 
+			throw_burger()
+
 		#current_burger.global_position.x = get_viewport().get_global_mouse_position().x 
 		#current_burger.global_position.y = get_viewport().get_mouse_position().y ##These are local coords
-		#var dir_ray : Dictionary = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.project_ray_origin(event.position),\
-			 #camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)))
-		#if !dir_ray: 
-			#mouse_position = camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)
-		#else:
-			#mouse_position = dir_ray["position"]
-		##swipe_direction = Vector2.UP.direction_to(Vector2(mouse_position.x, mouse_position.y))
+		
+		#swipe_direction = Vector2.UP.direction_to(Vector2(mouse_position.x, mouse_position.y))
 		#print((Vector2.UP.direction_to(Vector2(mouse_position.x, mouse_position.y))))
 			#current_burger.look_at((dir_ray.position - current_burger.global_transform.origin).normalized(), Vector3.UP)
 		
@@ -66,7 +62,7 @@ func throw_burger() -> void:
 	
 	var direction = (mouse_pos - Vector2i(screen_center))
 	
-	var angle = atan2(direction.x, -direction.y)  # Negative y because forward is -z in godot
+	var angle = atan2(direction.x, direction.y)
 	
 	#if angle > -PI/2 and angle < PI/2: angle += 3*PI/2
 	current_burger.rotation.y = angle
@@ -81,14 +77,26 @@ func throw_burger() -> void:
 	
 	burger_thrown = true
 	
+	if %BurgerRespawnTimer.is_stopped(): %BurgerRespawnTimer.start()
+	
+	charging_throw = false
 	
 	#%BurgerRespawnTimer.start(); await %BurgerRespawnTimer.timeout
 	
+##Extra info: https://stackoverflow.com/questions/76893256/how-to-get-the-3d-mouse-pos-in-godot-4-1 or the yt vid
+func shoot_ray(event: InputEvent):
+	var dir_ray : Dictionary = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.project_ray_origin(event.position),\
+			 camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)))
+	if !dir_ray: 
+		mouse_position = camera.project_ray_origin(event.position) + (camera.project_ray_normal(event.position) * camera.far)
+		return {"position": mouse_position}
+	else:
+		return dir_ray
 
-func _process(delta: float) -> void:
-	if charging_throw:
+#func _process(delta: float) -> void:
+	#if charging_throw:
 		#throw_power = throw_power * ((delta + 100)/2)
-		current_burger.gravity_scale = -0.1
+		#current_burger.gravity_scale = -0.1
 	#else:
 		#throw_power = 1 #TODO: add it on new burger spawn
 		#current_burger.gravity_scale = 0.6
