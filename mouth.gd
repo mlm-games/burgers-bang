e6cdbb6b888c4b12b95848a74207a243ea010b2d
mlm-studios <you@example.com>
2025-03-31 12:13:10 +0530

    add proper left and right movement after 10 throws

--- mouth.gd
+++ mouth.gd
@@ -1,8 +1,12 @@
 class_name BurgerFillers extends Area3D
 
-static var move_distance : float = 0.1
+const MAX_TRAVEL_WIDTH = 2
 
+static var move_distance : float = 0.1
+static var move_continously: bool = false
+static  var tween: Tween
 static var later_move_speed: float = 0.25
+static var moving_right : bool = true
 @export var move_max_distance: float
 
 static var rand_mov_dir : Vector3:
@@ -10,9 +14,22 @@ static var rand_mov_dir : Vector3:
 
 @onready var init_pos := global_position
 
+func _process(delta: float) -> void:
+	if move_continously and !tween.is_running():
+		if !global_position.x >= MAX_TRAVEL_WIDTH and moving_right:
+			global_position += (Vector3.RIGHT * delta)
+		else:
+			moving_right = false
+		
+		if !global_position.x <= -MAX_TRAVEL_WIDTH and !moving_right:
+			global_position += (Vector3.LEFT * delta)
+		else:
+			moving_right = true
+
+
 func on_burger_hit() -> void:
 	World.burgers_landed += 1
-	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	if World.burgers_landed > 3:
 		tween.tween_property(self, "global_position", init_pos + rand_mov_dir * move_distance, 0.5)
@@ -20,8 +37,4 @@ func on_burger_hit() -> void:
 	if World.burgers_landed > 6:
 		move_distance = minf(move_distance+0.1, 1.4)
 	if World.burgers_landed > 10:
-		var tween2 = get_tree().create_tween().set_loops(10)
-		if global_position.y == get_viewport().size.y - 20:
-			tween2.tween_property(self, "global_position", global_position + Vector3.LEFT/10, 0.05)
-		else:
-			tween2.tween_property(self, "global_position", global_position + Vector3.RIGHT/10, 0.05)
+		move_continously = !move_continously
